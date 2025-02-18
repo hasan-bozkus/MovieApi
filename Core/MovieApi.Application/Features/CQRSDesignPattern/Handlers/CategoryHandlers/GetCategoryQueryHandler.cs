0d94cc548481c7df6bfe476e224ca5ebf1d20d76
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-02-18 14:03:36 +0300

    Movie Api - AspNet Core 9.0 Onion Architecture CQRS & Mediator
    Ders 9: Get Movie Command Handler Sınıfı

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/CategoryHandlers/GetCategoryQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesginPattern.Results.CategoryResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers
+{
+    public class GetCategoryQueryHandler 
+    {
+        private readonly MovieContext _movieContext;
+
+        public GetCategoryQueryHandler(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
+        public async Task<List<GetCategoryQueryResult>> Handle()
+        {
+            var values = await _movieContext.Categories.ToListAsync();
+            return values.Select(x => new GetCategoryQueryResult
+            {
+                CategoryId = x.CategoryId,
+                CategoryName = x.CategoryName
+            }).ToList();
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.CQRSDesginPattern.Results.MovieResults;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class GetMovieQueryHandler
+    {
+        private readonly MovieContext _movieContext;
+
+        public GetMovieQueryHandler(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
+        public async Task<List<GetMovieQueryResult>> Handle()
+        {
+            var values = await _movieContext.Movies.ToListAsync();
+            return values.Select(x => new GetMovieQueryResult
+            {
+                MovieId = x.MovieId,
+                Title = x.Title,
+                Description = x.Description,
+                CoverImageUrl = x.CoverImageUrl,
+                Duration = x.Duration,
+                Rating = x.Rating,
+                ReleaseDate = x.ReleaseDate,
+                CreatedYear = x.CreatedYear,
+                Status = x.Status
+            }).ToList();
+        }
+    }
+}
