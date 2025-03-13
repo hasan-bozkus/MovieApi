783190baeef6620e698535ddde9c4185be4d57e7
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-03-13 22:09:32 +0300

    Movie Api - AspNet Core 9.0 Onion Architecture CQRS & Mediator
    Ders 15: Mediator Nedir?
    Ders 16: Commands Queries Results Sınıfları
    Ders 17: Mediator Handler Sınıfları
    Ders 18: Meditor Cast Query Handler

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/CreateCastCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Command.CastCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    public class CreateCastCommandHandler : IRequestHandler<CreateCastCommand>
+    {
+        private readonly MovieContext _movieContext;
+
+        public CreateCastCommandHandler(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
+        public async Task Handle(CreateCastCommand request, CancellationToken cancellationToken)
+        {
+            _movieContext.Casts.Add(new Cast()
+            {
+                Title = request.Title,
+                Namee = request.Name,
+                Surname = request.Surname,
+                ImageUrl = request.ImageUrl,
+                Overview = request.Overview,
+                Biography = request.Biography
+            });
+            await _movieContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/UpdateCastCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Command.CastCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+{
+    public class UpdateCastCommandHandler : IRequestHandler<UpdateCastCommand>
+    {
+        private readonly MovieContext _movieContext;
+
+        public UpdateCastCommandHandler(MovieContext movieContext)
+        {
+            _movieContext = movieContext;
+        }
+
+        public async Task Handle(UpdateCastCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _movieContext.Casts.FindAsync(request.CastId);
+            values.Namee = request.Namee;
+            values.Surname = request.Surname;
+            values.Overview = request.Overview;
+            values.Biography = request.Biography;
+            values.ImageUrl = request.ImageUrl;
+            values.Title = request.Title;
+            await _movieContext.SaveChangesAsync();
+        }
+    }
+}
