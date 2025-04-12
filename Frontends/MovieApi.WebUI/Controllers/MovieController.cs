3842401fb59a6b9d853a0fdb5ef84caa2231a76d
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2025-04-12 13:25:18 +0300

    Movie Api - AspNet Core 9.0 Onion Architecture CQRS & Mediator
    Ders 25: Navbar Türkçeleştirme ve Düzenleme İşlemleri
    Ders 26: Movie List Sayfası
    Ders 27: Layout İşlemlerinin Tamamlanması

---
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult MovieList()
+        {
+            ViewBag.movieList = "Film Listesi";
+            ViewBag.conrollerName = "Ana Sayfa";
+            ViewBag.movieListing = "Tüm Filmler";
+            return View();
+        }
+    }
+}
