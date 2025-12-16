81467ca75f0f2b8a81fb0f79ef280bfb0b6db59e
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2025-12-16 20:21:52 +0300

    Movie Api - AspNet Core 9.0 Onion Architecture CQRS & Mediator
    Ders 38: Stitch AI ile Login Sayfası Tasarımı Oluşturma
    Ders 39: Anthropic Claude ile Admin Layout Düzenleme

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/ViewComponents/AdminLayoutComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Areas.Admin.ViewComponents.AdminLayoutComponents
+{
+    public class _AdminLayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
