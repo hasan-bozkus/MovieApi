3e0e578918d288e2c1b088f626e1a2a7a1f052e7
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2026-03-12 14:00:50 +0300

    Movie Api - AspNet Core 9.0 Onion Architecture CQRS & Mediator
    Ders 61: Strategy Design Pattern ve Cast Sayfası

---
+++ Frontends/MovieApi.WebUI/Areas/Admin/Controllers/AdminCastController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Areas.Admin.Controllers
+{
+    public class AdminCastController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCastController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult CastList()
+        {
+            return View();
+        }
+    }
+}
